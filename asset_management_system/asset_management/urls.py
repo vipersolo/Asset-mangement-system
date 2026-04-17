3edadaa91d9e47883e9dbe5a062b3d3998024350
Antony Jose <antonyjose777aj@gmail.com>
2026-04-17 21:30:08 +0530

    asset management system

--- asset_management_system/asset_management/urls.py
+++ asset_management_system/asset_management/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for asset_management_system project.
+URL configuration for asset_management project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
+    path('', include('core.urls')),
     path('admin/', admin.site.urls),
+    
 ]
